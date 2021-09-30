233e742c123e14d020e42e47d93b15b6a9ab8f54
SushrutD-Prorigo <sushrut.deore@prorigosoftware.com>
2021-09-30 17:53:05 +0530

    Day-4

--- LibraryManagement/src/main/webapp/demo.js
+++ LibraryManagement/src/main/webapp/demo.js
@@ -3,8 +3,68 @@
  */
 
 
-function greet(name)
+
+var name="vcedvev";
+function greet()
 {
+	 var name=document.getElementById("myName").value;
 	document.getElementById("greet").innerHTML="Welcome"+" "+name;
+}
+
+
+console.log("Welcome to js");
+
+var arr=["Honda","Kawasaki","Hero","TVS"];
+
+console.log(arr);
+console.log("\n");
+for(let i=0;i<arr.length;i++)
+{
+	console.log(arr[i]);
+}
+
+console.log(arr.concat(["Sushrut","Deore"]));
+
+console.log(arr.unshift("BMW"));
+console.log(arr.push("Tesla"));
+console.log(arr.pop());
+console.log(arr);
+
+
+function modifyArray(arr)
+{
+	for(let i=0;i<arr.length;i++)
+		{
+			if(arr[i]=="BMW")
+				arr[i]="newBMW";
+		}
+	
+	console.log(arr);	
+}
+
+modifyArray(arr);
+/*
+var age=18;
+	
+	if(age >= 22)
+		console.log("Eligible for voting and can also Nominate for Election");
+	else if(age>= 18)
+		console.log("Eligible for Voting");
+	else
+		console.log("Come after "+(18-age) +" Years for voting");
+
+*/
+
+function validateAge()
+{
+	var age=parseInt(document.getElementById("age").value);
+	
+	if(age >= 22)
+		document.getElementById("validate").innerHTML="Eligible for voting and can also Nominate for Election";
+	else if(age>= 18)
+		document.getElementById("validate").innerHTML="Eligible for Voting";
+	else
+		document.getElementById("validate").innerHTML="Come after "+(18-age) +" Years for voting";
+
 }
 
